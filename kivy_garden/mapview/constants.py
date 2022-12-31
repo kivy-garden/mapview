8eb674456699123fc4bc5e82e6ce6c61c5a947df
Paul WOISARD <paulwoisard@gmail.com>
2022-12-31 08:57:25 +0100

    Mise à Jour Cache Multi OS

--- kivy_garden/mapview/constants.py
+++ kivy_garden/mapview/constants.py
@@ -1,6 +1,12 @@
 import os
+import platform
 MIN_LATITUDE = -90.0
 MAX_LATITUDE = 90.0
 MIN_LONGITUDE = -180.0
 MAX_LONGITUDE = 180.0
-CACHE_DIR = os.getenv("HOME") + 'Library/Low-Fuel/'
+if platform.system() == 'Darwin':
+    CACHE_DIR = os.getenv("HOME") + '/Library/Low-Fuel/'
+if platform.system() == 'Linux':
+    CACHE_DIR = '/tmp/Low-Fuel/'
+if platform.system() == 'Windows':
+    CACHE_DIR = os.getenv("HOME") + '\\AppData\\Local\\Temp\\Low-Fuel\\' 
